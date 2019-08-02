60125aca4a2896b611c1ecf62307df091cebe991
Greg <gmerrill03@gmail.com>
2019-08-02 14:16:31 -0700

    add mineLocations function

--- js/main.js
+++ js/main.js
@@ -11,6 +11,59 @@ const COLORS = {
 };
 const offset = 8;
 
+class Tile {
+    constructor(location, status) {
+        // references both the dom id and the array id
+        this.location = location;
+        this.status = status;
+    }
+    getAllValidNeighbors() {
+        // function that returns array of locations of all valid neighboring tiles (valid meaning: they should be revealed if blank / checked if a number tile)
+    }
+}
+class blank extends Tile {
+    constructor(location, status) {
+        super(location, status);
+        this.type = 'blank';
+    }
+    // any special functions needed by blank tiles
+    getBlankNeighbors() {
+        // function that returns location of any blank tiles touching this one on the top, bottom, left, or right
+    }
+}
+class number extends Tile {
+    constructor(location, status) {
+        super(location, status);
+        this.type = 'number';
+    }
+    // any special functions needed by number tiles
+    getMineNeighbors(){
+        // function that returns the number of mines this tile is touching
+    }
+}
+class mine extends Tile {
+    constructor(location, status) {
+        super(location, status);
+        this.type = 'mine';
+    }
+    // any special functions needed by mine tiles
+}
+
+// ******* SANDBOX ********
+let tiles = [];
+
+
+
+
+
+// ******************
+
+
+// const tile = {
+//     tileStatus: 'hidden'
+// };
+
+
 // state variables
 let board = [               // r
      0,  0,  2, -1,  0,  0,  0,  0,  // 0
@@ -24,6 +77,7 @@ let board = [               // r
 //c 0  1  2  3  4  5  6  7
 ];
 let playerStatus = 'playing';  // won, lost, or playing
+let numberOfMines = 10;
 
 // cached elements
 
@@ -59,7 +113,7 @@ function render(id) {
     }
     // write number inside div using innerHTML
     if (board[id] === 0) {
-        // // change background of all touching tiles around it
+        // // change background of all touching tiles around blank tiles no matter the board value
         document.getElementById(`${id - offset - 1}`).style.backgroundColor = 'lightgray';
         document.getElementById(`${id - offset}`).style.backgroundColor = 'lightgray';
         document.getElementById(`${id - offset + 1}`).style.backgroundColor = 'lightgray';
@@ -72,6 +126,7 @@ function render(id) {
         // //  and display board value in divs of all touching tiles that have a board value !== 0
         let i, indexValue, boardValue, colorIndex;
         for (i = -1; i <= 1; i++) {
+            // render any numbers above blank tile
             if (board[id - offset + i] !== 0) {
                 indexValue = id - offset + i;
                 boardValue = board[indexValue];
@@ -79,6 +134,7 @@ function render(id) {
                 document.getElementById(`${indexValue}`).style.color = COLORS[colorIndex];
                 document.getElementById(`${indexValue}`).innerHTML = `${boardValue}`;
             }
+            // render any numbers below blank tile
             if (board[id + offset + i] !== 0) {
                 indexValue = id + offset + i;
                 boardValue = board[indexValue];
@@ -88,6 +144,7 @@ function render(id) {
             }
         }
         for (i = -1; i <= 1; i += 2) {
+            // render any numbers to left and right of blank tile
             if (board[id + i] !== 0) {
                 indexValue = id + i;
                 boardValue = board[indexValue];
@@ -111,3 +168,19 @@ function handleClick(evt) {
     let id = parseInt(evt.target.id);
     render(id);
 };
+
+function mineLocations(numberOfMines) {
+    let locationOfMines = Array(numberOfMines).fill('to be filled');
+    let prevMineLocations = [];
+    locationOfMines.forEach((val, idx) => {
+        let randLocation = Math.floor(Math.random() * board.length);
+        // while loop: keep reassigning a new location if location = any number in prevMineLocations
+        while (prevMineLocations.some(value => value === randLocation)) {
+            randLocation = Math.floor(Math.random() * board.length);
+        }
+        prevMineLocations.push(randLocation);
+        locationOfMines[idx] = randLocation;
+    });
+    return locationOfMines;
+}
+console.log(mineLocations(numberOfMines));
